bdb07b215112a752b35e21555a8578da95a83569
YouD0313 <102004480+YouD0313@users.noreply.github.com>
2023-01-17 16:32:52 +0900

    show trasition

--- js/more.js
+++ js/more.js
@@ -8,18 +8,28 @@ const arrowLeft = document.querySelector('.left');
 // const slidelist = document.querySelector('.slidelist');
 // console.log('slideItem.attributes[1].value', slideItem.attributes[1].value);
 
+// window.addEventListener('DOMContentLoaded', function () {
+// 	$(infoMore).hide();
+// });
+window.addEventListener('DOMContentLoaded', function () {
+	// const date = new Date();
+	// const milliseconds = date.getMilliseconds();
+	// console.log(date);
+	// console.log(milliseconds);
+	// for (let i = 0; i < 11; i++) {
+	// 	let opacityS = i / 10;
+	// 	console.log(opacityS);
+	// }
+});
+
 clickMore.addEventListener('click', function () {
-	// let createDiv = document.createElement('div');
-	// slidelist.appendChild(createDiv);
 	infoMore.classList.toggle('block');
 	if (infoMore.classList.value == 'infoMore block') {
 		clickMore.classList.remove('fa-caret-up');
 		clickMore.classList.add('fa-caret-down');
-		// createDiv.classList.add('height');
 	} else if (infoMore.classList.value == 'infoMore') {
 		clickMore.classList.add('fa-caret-up');
 		clickMore.classList.remove('fa-caret-down');
-		// slidelist.removeChild(document.querySelector('.height'));
 	}
 });
 $('.slidelist').slick({
